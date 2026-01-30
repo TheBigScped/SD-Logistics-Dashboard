efbd859e09b5e2b3e2e3f9ff2f590eaa603cb06e
Olly <oly123abc@gmail.com>
2026-01-30 03:06:41 +0000

    Add basic navigation and custom 404 page

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ app = Flask(__name__, template_folder="app/templates")
 def dashboard():
     return render_template("dashboard.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/status")
+def status():
+    return render_template("status.html")
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template("base.html"), 404
+    return render_template("404.html"), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
